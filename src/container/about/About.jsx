769f7e6cd0d3e93529aace511aaa3e2f5779ef6a
hasti-akd <hasti.akbarideilami@georgebrown.ca>
2023-03-22 19:11:31 -0400

    Completing About page

--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1 className="text-white">Under Construction!</h1>
-      <h1 className="text-white">Need content!</h1>
+    <div class="about">
+      <h1 className="text-white">Our Story</h1>
+      <br></br>
+      <p>Welcome to HeartBeats, the ultimate web application designed to enhance your music streaming experience with personalized music recommendations based on your emotional preferences. We leverage the power of the Spotify API to analyze your listening history, behavior, and preferences to provide you with tailored music recommendations that resonate with your emotions.</p>
+      <p>Our mission is to deepen the connection between your feelings and the music you listen to, making HeartBeats an ideal choice for anyone who values music as a source of emotional support and comfort. Whether you want to unwind after a stressful day or search for a playlist to boost your energy levels, HeartBeats has got you covered.</p>
+      <p>Our platform provides highly accurate music recommendations catering to your unique emotional preferences. We believe that the right music can profoundly impact your mental and emotional well-being, and our goal is to help you discover the music that resonates with your soul. With HeartBeats, there is no need to sign up; connect your Spotify account and start discovering your personalized music recommendations today.</p>
+      <p>So why wait? Embark on a journey of personalized music recommendations that will enhance your music streaming experience and enrich your life with HeartBeats.</p>
+      <p>• Want to find out more?</p>
+      <p>If you have any enquiries and would like to get in touch with us, please drop us an email at <a href="mailto:hello@heartbeats.com">hello@heartbeats.com</a>.</p>
+      <br></br>
     </div>
   );
 }
