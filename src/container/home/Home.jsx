8299b4ff8ca0929741ba0be8ba11d379e8e98076
hasti-akd <hasti.akbarideilami@georgebrown.ca>
2023-03-26 02:15:51 -0400

    Started working on the mood bar

--- src/container/home/Home.jsx
+++ src/container/home/Home.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Home() {
-  return <div style={{ color: "white" }}>Home</div>;
+  return ( <div>
+    <div class="slides">
+        <p>If your mood was a song, what would it be?</p>
+        <div class="slides-navigation">
+          <ul>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Sad</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Sad</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Calm</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Calm</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Calm</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Energetic</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Energetic</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Energetic</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Happy</div></a></li>
+            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Happy</div></a></li>
+          </ul>
+        </div>
+    </div>
+  </div>
+  );
 }
