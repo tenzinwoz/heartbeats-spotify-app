efc14847b4fea587960cfb97c41246ab12bd28c8
hasti-akd <hasti.akbarideilami@georgebrown.ca>
2023-03-26 23:26:11 -0400

    Completing the mood bar and adding trending part

--- src/container/home/Home.jsx
+++ src/container/home/Home.jsx
@@ -1,24 +1,151 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  return ( <div>
-    <div class="slides">
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return ( 
+    <div>
+      <div className="slides">
         <p>If your mood was a song, what would it be?</p>
         <div class="slides-navigation">
           <ul>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Sad</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Sad</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Calm</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Calm</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Calm</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Energetic</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Energetic</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Energetic</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Happy</div></a></li>
-            <li class="slides-navigation-item"><a href="/"><span></span><div class="tooltip">Happy</div></a></li>
+          <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Sad
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Sad
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Calm
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Calm
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Calm
+                </div>
+              </a>
+            </li>
+
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Energetic
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Energetic
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Energetic
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Happy
+                </div>
+              </a>
+            </li>
+            <li className="slides-navigation-item">
+              <a
+                href="/"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <span></span>
+                <div className={`tooltip ${showTooltip ? "show" : ""}`}>
+                  Happy
+                </div>
+              </a>
+            </li>
           </ul>
+          <span class="scale-label scale-label-left">01</span>
+          <span class="scale-label scale-label-right">10</span>
         </div>
+      </div>
+      <div className="trending">
+        <p>Trending songs</p>
+      </div>
     </div>
-  </div>
   );
 }
